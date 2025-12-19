e685b15e6945520b9ab7f51cbb95fad004708709
Cory0726 <lkfu520@gmail.com>
2025-12-19 09:27:41 +0800

    update

--- imgs_processing_workspace.py
+++ imgs_processing_workspace.py
@@ -1,51 +1,75 @@
+import os
+import shutil
 import cv2
 
 import tools_box
 
+dir_home = 'Origin'
+dir = ['Origin_EgoHands', 'Origin_EgoYouTubeHands', 'Origin_EGTEA_GAZE_PLUS', 'Origin_GTEA', 'Origin_GTEA_GAZE_PLUS', 'Origin_HandOverFace']
 
 def main():
-    # dir = ['Origin_EgoHands', 'Origin_EgoYouTubeHands', 'Origin_EGTEA_GAZE_PLUS', 'Origin_GTEA', 'Origin_GTEA_GAZE_PLUS', 'Origin_HandOverFace']
-    # for i in range(6):
-        # tools_box.center_crop_to_square(f'{dir[i]}/imgs', f'{dir[i]}/imgs_cropped')
-        # tools_box.center_crop_to_square(f'{dir[i]}/masks', f'{dir[i]}/masks_cropped')
 
-        # tools_box.convert_folder_to_grayscale(f'{dir[i]}/imgs_cropped', f'{dir[i]}/imgs_grayscale')
-        # tools_box.binarize_images(f'{dir[i]}/masks_cropped', f'{dir[i]}/masks_binarized')
+    for i in range(6):
+        dir[i] = f'{dir_home}/{dir[i]}'
+        # Crop(Imgs and Masks)
+        tools_box.center_crop_to_square(f'{dir[i]}/imgs', f'{dir[i]}/imgs_cropped')
+        tools_box.center_crop_to_square(f'{dir[i]}/masks', f'{dir[i]}/masks_cropped')
+        # Masks binarize
+        tools_box.binarize_images(f'{dir[i]}/masks_cropped', f'{dir[i]}/masks_binarized')
+        # Filter
+        tools_box.dataset_mask_filter(
+            image_dir=f'{dir[i]}/imgs_cropped',
+            mask_dir=f'{dir[i]}/masks_binarized',
+            mask_ratio_range=(0.02, 0.4)
+        )
+        # Resize
+        tools_box.resize_and_convert_image(
+            input_folder=f'{dir[i]}/imgs_cropped',
+            output_folder=f'{dir[i]}/imgs_resized',
+            target_size=(480, 480),
+        )
+        tools_box.resize_and_convert_mask(
+            input_folder=f'{dir[i]}/masks_binarized',
+            output_folder=f'{dir[i]}/masks_resized',
+            target_size=(480, 480),
+        )
+        # GrayScale
+        tools_box.convert_folder_to_grayscale(f'{dir[i]}/imgs_resized', f'{dir[i]}/imgs_grayscale')
+        # Remove temp dir
+        shutil.rmtree(f'{dir[i]}/imgs')
+        shutil.rmtree(f'{dir[i]}/imgs_cropped')
+        shutil.rmtree(f'{dir[i]}/imgs_resized')
+        shutil.rmtree(f'{dir[i]}/masks')
+        shutil.rmtree(f'{dir[i]}/masks_cropped')
+        shutil.rmtree(f'{dir[i]}/masks_binarized')
+        # Rename
+        os.rename(f'{dir[i]}/imgs_grayscale', f'{dir[i]}/imgs')
+        os.rename(f'{dir[i]}/masks_resized', f'{dir[i]}/masks')
 
-        # tools_box.resize_and_convert_image(
-        #     input_folder=f'{dir[i]}/imgs_grayscale',
-        #     output_folder=f'{dir[i]}/imgs_resized',
-        #     target_size=(480, 480),
-        # )
-        # tools_box.resize_and_convert_mask(
-        #     input_folder=f'{dir[i]}/masks_binarized',
-        #     output_folder=f'{dir[i]}/masks_resized',
-        #     target_size=(480, 480),
-        # )
 
-        # tools_box.dataset_mask_filter(
-        #     image_dir=f'{dir[i]}/imgs_resized',
-        #     mask_dir=f'{dir[i]}/masks_resized',
-        #     mask_ratio_range=(0.1, 0.4)
-        # )
-        # dir = 'Process_HandOverFace_480_480_MaskFilter_10_40_GrayScale'
-        # dir_output = 'Process_All_Hands_480_480_MaskFilter_10_40_GrayScale'
-        # num = 6302
-        # tools_box.rename_and_move_files(
-        #     input_dir=f'{dir}/imgs_resized',
-        #     output_dir=f'{dir_output}/imgs',
-        #     start_num=num,
-        # )
-        # tools_box.rename_and_move_files(
-        #     input_dir=f'{dir}/masks_resized',
-        #     output_dir=f'{dir_output}/masks',
-        #     start_num=num,
-        #     suffix='_mask'
-        # )
 
-    img = cv2.imread('Process_All_Hands_480_480_MaskFilter_10_40_GrayScale/imgs_grayscale/img_00031.png', cv2.IMREAD_UNCHANGED)
-    print(img.shape)
-    img_mask = cv2.imread('Process_All_Hands_480_480_MaskFilter_10_40_GrayScale/masks/img_00001_mask.png', cv2.IMREAD_UNCHANGED)
-    print(img_mask.shape)
+def main_2():
+
+    dir_output = 'Process_All_Hands_480_480_MaskFilter_02_40_GrayScale'
+
+    for i in range (6):
+        dir[i] = f'{dir_home}/{dir[i]}'
+        dir_items = os.listdir(f'{dir_output}/imgs')
+        total_dir_items = len(dir_items)
+        print(f'Total dir items: {total_dir_items}')
+        num = total_dir_items + 1
+
+        tools_box.rename_and_move_files(
+            input_dir=f'{dir[i]}/imgs',
+            output_dir=f'{dir_output}/imgs',
+            start_num=num,
+        )
+        tools_box.rename_and_move_files(
+            input_dir=f'{dir[i]}/masks',
+            output_dir=f'{dir_output}/masks',
+            start_num=num,
+            suffix='_mask'
+        )
+
 if __name__ == '__main__':
-    main()
+    main_2()
